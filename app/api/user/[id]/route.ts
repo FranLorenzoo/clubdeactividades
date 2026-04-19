e5c501b2d85455ee5cb2fdc6e22e1b726ab1c799
FranLorenzo <jfranlorenzo@gmail.com>
2026-04-19 19:29:36 -0300

    feat: prisma client and user api

---
+++ app/api/user/[id]/route.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+   const { id } = await params
+
+  console.log("ID:", id)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id)
+    },
+  })
+
+  if (!user) {
+    return NextResponse.json({ error: 'User not found' }, { status: 404 })
+  }
+
+  return NextResponse.json(user)
+}
