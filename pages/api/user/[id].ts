94b56faa568ca647af4fec9691ca4046d8d0afb2
FranLorenzo <jfranlorenzo@gmail.com>
2026-04-19 19:57:28 -0300

    refactor: app to pages file structure

---
+++ pages/api/user/[id].ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) { 
+  const { id } = req.query; 
+  console.log( JSON.stringify(req, null, 2)) 
+  if (req.method === "GET") { 
+    try { 
+        const user = await prisma.user.findUnique({ 
+          where: { 
+            id: parseInt(id as any) 
+          }, 
+        }); 
+        if (!user) { 
+          return res.status(404).json({ message: "User not found" }); 
+        } 
+        res.status(200).json(user); 
+      } catch (error) { 
+        res.status(500).json({ message: "Internal server error" }); 
+      } 
+  } else { 
+    res.status(405).json({ message: "sos puto" }); 
+  } 
+}
---
+++ pages/api/user/index.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) { 
+  if (req.method === "GET") { 
+    try { 
+      const user = await prisma.user.findMany(); 
+      if (!user) { 
+        return res.status(404).json({ message: "User not found" }); 
+      } 
+      res.status(200).json(user); 
+    } catch (error) { 
+      res.status(500).json({ message: "Internal server error" }); 
+    } 
+  } else { 
+    res.status(405).json({ message: "sos puto" }); 
+  } 
+}
