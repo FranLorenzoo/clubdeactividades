9d2b6dd7270950a0e199f026fce4304c9290692e
Mateo Drake <mateodrake04@outlook.com.ar>
2026-05-16 13:47:53 -0300

    acomodando imagenes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,11 @@ import "@/pages/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (<>
+    <head>
+       <title>Club360</title>
+    </head>
+    <Component {...pageProps} />;
+  </>);
+
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/layout/Hero";
 import Activities from "@/components/activities";
 import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950 text-white">
+      <Navbar />
       <Hero />
       <Activities />
       <Footer />
