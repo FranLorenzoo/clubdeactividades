fcadd67d5562d9d95089d352acfa1c266ac5c0d9
Mateo Drake <mateodrake04@outlook.com.ar>
2026-05-25 18:17:07 -0300

    acomodando color del nav-bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
     <header className="border-b border-zinc-800">
       <div className="max-w-7xl mx-auto px-6 py-5 flex justify-between items-center">
 
-        <Link href="/" className="text-2xl font-bold tracking-wide">
+        <Link href="/" className="text-2xl text-white font-bold tracking-wide">
           Club<span className="text-green-500">360</span>
         </Link>
 
@@ -42,13 +42,13 @@ export default function Navbar() {
             <>
               <Link
                 href={panelUrl}
-                className="px-5 py-2 rounded-xl border border-zinc-700 hover:bg-zinc-800 transition"
+                className="px-5 py-2 text-white rounded-xl border border-zinc-700 hover:bg-zinc-800 transition"
               >
                 Mi panel
               </Link>
               <button
                 onClick={handleLogout}
-                className="px-5 py-2 rounded-xl border border-zinc-700 hover:bg-zinc-800 transition"
+                className="px-5 py-2 text-white rounded-xl border border-zinc-700 hover:bg-zinc-800 transition"
               >
                 Cerrar sesión
               </button>
@@ -57,14 +57,14 @@ export default function Navbar() {
             <>
               <Link
                 href="/login"
-                className="px-5 py-2 rounded-xl border border-zinc-700 hover:bg-zinc-800 transition"
+                className="px-5 py-2 rounded-xl border text-white border-zinc-700 hover:bg-zinc-800 transition"
               >
                 Iniciar sesión
               </Link>
 
               <Link
                 href="/register"
-                className="px-5 py-2 rounded-xl bg-green-600 hover:bg-green-700 transition"
+                className="px-5 py-2 text-white rounded-xl bg-green-600 hover:bg-green-700 transition"
               >
                 Registrarse
               </Link>
